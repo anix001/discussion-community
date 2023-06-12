db70f1c1bb6cfacdcff0db334c418f44edd4a94b
anix001 <chaudharyanish305@gmail.com>
2023-06-12 14:14:59 +0545

    Configuration: Swagger Config

--- src/main/java/com/DiscussionCommunity/DiscussionCommunityApplication.java
+++ src/main/java/com/DiscussionCommunity/DiscussionCommunityApplication.java
@@ -3,9 +3,11 @@ package com.DiscussionCommunity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableAsync
+@EnableWebMvc
 public class DiscussionCommunityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/DiscussionCommunity/security/SecurityFilterChainConfig.java
+++ src/main/java/com/DiscussionCommunity/security/SecurityFilterChainConfig.java
@@ -22,13 +22,17 @@ public class SecurityFilterChainConfig {
         this.authenticationProvider = authenticationProvider;
         this.jwtAuthFilter = jwtAuthFilter;
     }
+
+    public static final String[] PUBLIC_URLS = {
+            "/auth/**", "/v3/api-docs", "/v2/api-docs", "/swagger-resources/**", "/swagger-ui/**","/webjars/**"
+    };
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws  Exception{
         http
                 .csrf()
                 .disable()
                 .authorizeHttpRequests()
-                .antMatchers("/auth/**")
+                .antMatchers(PUBLIC_URLS)
                 .permitAll()
                 .antMatchers(HttpMethod.GET, "/api/v1/posts/**")
                 .permitAll()
